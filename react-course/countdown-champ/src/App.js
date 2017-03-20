5ca15a9d931973ec2f8d0f25fa04281ba4274810
Travis Alcantara <travis.alcantara@gmail.com>
2017-03-20 16:00:24 -0700

    completed project 1

--- react-course/countdown-champ/src/App.js
+++ react-course/countdown-champ/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 
 class App extends Component {
@@ -24,13 +25,14 @@ class App extends Component {
             deadline={this.state.deadline}
           />
         </div>
-        <div>
-          <input
+        <Form inline>
+          <FormControl
+          className="Deadline-input"
           placeholder='new date'
           onChange={event => this.setState({newDeadline:event.target.value})}
           />
-          <button onClick= {() => this.changeDeadline()}>Submit</button>
-        </div>
+          <Button onClick= {() => this.changeDeadline()}>Submit</Button>
+        </Form>
     </div>
   )
   }
--- react-course/countdown-champ/src/Clock.js
+++ react-course/countdown-champ/src/Clock.js
@@ -19,6 +19,9 @@ class Clock extends Component {
   componentDidMount() {
     setInterval(() => this.getTimeUntil(this.props.deadline), 1000)
   }
+  leading0(num) {
+    return num < 10 ? '0' + num : num;
+  }
   getTimeUntil(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
     const seconds = Math.floor((time/1000)%60);
@@ -31,10 +34,10 @@ class Clock extends Component {
   render() {
     return (
       <div>
-        <div className="Clock-days">{this.state.days} days</div>
-        <div className="Clock-hours">{this.state.hours} hours</div>
-        <div className="Clock-minutes">{this.state.minutes} minutes</div>
-        <div className="Clock-seconds">{this.state.seconds} seconds</div>
+        <div className="Clock-days">{this.leading0(this.state.days)} days</div>
+        <div className="Clock-hours">{this.leading0(this.state.hours)} hours</div>
+        <div className="Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+        <div className="Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
       </div>
     )
   }

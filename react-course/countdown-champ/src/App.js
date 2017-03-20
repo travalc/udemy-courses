24af26daf45965e565e3e9b685c889ead0d61fae
Travis Alcantara <travis.alcantara@gmail.com>
2017-03-19 20:39:13 -0700

    completed up to updating state section

--- react-course/countdown-champ/src/App.js
+++ react-course/countdown-champ/src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    }
+  };
+  changeDeadline() {
+    console.log('state', this.state);
+    this.setState({deadline: this.state.newDeadline})
+  }
   render() {
     return (
-    <div>Countdown Champ, App Component</div>
+    <div className="App">
+      <div className="App-title">Countdown to {this.state.deadline}</div>
+        <div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
+        </div>
+        <div>
+          <input
+          placeholder='new date'
+          onChange={event => this.setState({newDeadline:event.target.value})}
+          />
+          <button onClick= {() => this.changeDeadline()}>Submit</button>
+        </div>
+    </div>
   )
   }
 }

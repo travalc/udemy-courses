39b59541316473c8fb40c4d63bc89951fbda0e3f
Travis Alcantara <travis.alcantara@gmail.com>
2017-03-21 16:48:47 -0700

    Completed sections up to CSS

---
+++ react-course/music-master/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Profile from './Profile'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      artist: null
+    }
+  }
+  search() {
+    console.log('this.state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    console.log(FETCH_URL);
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];
+      console.log('artist', artist);
+      this.setState({artist});
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+              />
+              <InputGroup.Addon onClick={() => this.search()}>
+                <Glyphicon glyph="search"></Glyphicon>
+              </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <Profile
+          artist ={this.state.artist}
+        />
+        <div className="Gallery">
+          Gallery
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;

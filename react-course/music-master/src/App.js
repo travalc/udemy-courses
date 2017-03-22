74bb513a1ca19a38ceac8a45c613ac0713a3f0c2
Travis Alcantara <travis.alcantara@gmail.com>
2017-03-22 12:06:59 -0700

    Finished through second project

--- react-course/music-master/src/App.js
+++ react-course/music-master/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
-import Profile from './Profile'
+import Profile from './Profile';
+import Gallery from './Gallery';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       query: '',
-      artist: null
+      artist: null,
+      tracks: []
     }
   }
   search() {
     console.log('this.state', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?';
-    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    const ALBUM_URL = 'https://api.spotify.com/v1/artists/'
     console.log(FETCH_URL);
     fetch(FETCH_URL, {
       method: 'GET'
@@ -24,7 +27,21 @@ class App extends Component {
       const artist = json.artists.items[0];
       console.log('artist', artist);
       this.setState({artist});
+
+      FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+      fetch(FETCH_URL, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => {
+        console.log('artist\'s top tracks:', json)
+        const { tracks } = json;
+        this.setState({tracks});
+      })
+
     });
+
+
   }
   render() {
     return (
@@ -48,12 +65,20 @@ class App extends Component {
               </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <Profile
-          artist ={this.state.artist}
-        />
-        <div className="Gallery">
-          Gallery
-        </div>
+        {
+          this.state.artist !== null
+          ?
+            <div>
+              <Profile
+                artist={this.state.artist}
+              />
+              <Gallery
+                tracks={this.state.tracks}
+              />
+            </div>
+          : <div></div>
+        }
+
       </div>
     )
   }

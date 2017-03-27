c95bb1c6fa3a11932e07ed73dd60d141cefa9009
Travis Alcantara <travis.alcantara@gmail.com>
2017-03-27 14:43:04 -0700

    Added toggle pause/play button functionality

--- react-course/lesson1-challenge-stopwatch/stopwatch/src/App.js
+++ react-course/lesson1-challenge-stopwatch/stopwatch/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
 import './App.css'
-import { Form, FormControl, Button } from 'react-bootstrap';
+import { Form, FormControl, Button, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
@@ -10,11 +10,14 @@ class App extends Component {
       minutes: 0,
       seconds: 0,
       enteredMinutes: 0,
-      enteredSeconds: 0
+      enteredSeconds: 0,
+      isCounting: true,
+      paused: false,
     }
   }
 
   changeMinutesAndSeconds() {
+    this.setState({paused: false});
     if (this.state.enteredSeconds < 60 && this.state.enteredMinutes < 60){
       const minutes = this.state.enteredMinutes;
       const seconds = this.state.enteredSeconds;
@@ -27,33 +30,57 @@ class App extends Component {
   }
 
   getTimeUntilZero() {
-    const minutes = this.state.minutes;
-    const seconds = this.state.seconds;
-    var newSeconds;
-    var newMinutes;
-    if (minutes < 1) {
-      newSeconds = seconds - 1;
-      this.setState({seconds: newSeconds});
-      if (minutes < 1 && seconds < 1) {
-        this.setState({minutes: 0, seconds: 0})
-      }
-    }
-    if (minutes > 0) {  
-      if (seconds > 0) {
+    if (this.state.paused === false) {
+      const minutes = this.state.minutes;
+      const seconds = this.state.seconds;
+      var newSeconds;
+      var newMinutes;
+      if (minutes < 1) {
         newSeconds = seconds - 1;
         this.setState({seconds: newSeconds});
+        if (minutes < 1 && seconds < 1) {
+          this.setState({minutes: 0, seconds: 0})
+        }
       }
-      else {
-        newSeconds = 59;
-        newMinutes = minutes - 1;
-        this.setState({minutes: newMinutes, seconds: newSeconds});
+      if (minutes > 0) {
+        if (seconds > 0) {
+          newSeconds = seconds - 1;
+          this.setState({seconds: newSeconds});
+        }
+        else {
+          newSeconds = 59;
+          newMinutes = minutes - 1;
+          this.setState({minutes: newMinutes, seconds: newSeconds});
+        }
       }
+
     }
+    else {
+      let currentMinutes = this.state.minutes;
+      let currentSeconds = this.state.seconds;
+      this.setState({minutes: currentMinutes, seconds: currentSeconds})
+      console.log(this.state);
+    }
+
   }
 
   componentDidMount() {
-
+    if (this.state.paused === false) {
       setInterval(() => this.getTimeUntilZero(), 1000);
+    }
+    else {
+
+    }
+
+  }
+
+  togglePause() {
+    if (this.state.paused === false) {
+      this.setState({paused: true});
+    }
+    else {
+      this.setState({paused: false});
+    }
 
   }
 
@@ -80,6 +107,13 @@ class App extends Component {
             onChange={event => this.setState({enteredSeconds:event.target.value})}
           />
           <Button onClick={() => this.changeMinutesAndSeconds()}>Start!</Button>
+          <Button onClick={() => this.togglePause()}>
+            {
+              this.state.paused === true
+              ? <span>&#9654;</span>
+              : <span>||</span>
+            }
+          </Button>
         </Form>
       </div>
     )

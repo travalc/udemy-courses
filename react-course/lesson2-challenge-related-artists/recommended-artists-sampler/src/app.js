0e0e5bf34b648c71fbc5f496c6334888b1d3c029
Travis Alcantara <travis.alcantara@gmail.com>
2017-03-28 14:12:14 -0700

    Added CSS/SCSS files, added link to bootstrap, added fetch methods to search function

--- react-course/lesson2-challenge-related-artists/recommended-artists-sampler/src/app.js
+++ react-course/lesson2-challenge-related-artists/recommended-artists-sampler/src/app.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import './css/App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchArtist: null,
+      relatedArtists: [],
+      query: ''
+    }
+  }
   search() {
+    const BASE_URL = 'https://api.spotify.com/v1/search';
+    let SEARCH_URL = `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
+    fetch(SEARCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];
+      this.setState({searchArtist: artist});
+      console.log(this.state.searchArtist);
+      let RA_URL = `https://api.spotify.com/v1/artists/${artist.id}/related-artists`;
+      fetch(RA_URL, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      })
+    })
+
 
   }
   render() {
@@ -10,13 +39,22 @@ class App extends Component {
       <div className="App">
         <div className="App-title">Recommended Artists Sampler</div>
         <FormGroup>
-          <FormControl
-            type="text"
-            placeholder="Enter an artist name"
-          />
-          <InputGroup.Addon>
-            <Glyphicon glyph="search"></Glyphicon>
-          </InputGroup.Addon>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Enter an artist name"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
         </FormGroup>
       </div>
     )

dc9a78a144aa5b823bb549468c2abebacc8594cd
Travis Alcantara <travis.alcantara@gmail.com>
2017-04-04 16:47:01 -0700

    Added save to cookies functionality

--- react-course/lesson2-challenge-todo-list/todo-list/src/reducers/index.js
+++ react-course/lesson2-challenge-todo-list/todo-list/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_TODO, DELETE_TODO, DELETE_ALL } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const todo = (action) => {
   return {
@@ -15,15 +16,19 @@ const deleteTodoById = (state = [], id) => {
 
 const todos = (state = [], action) => {
   let todos = null;
+  state = read_cookie('todos');
   switch (action.type) {
     case ADD_TODO:
       todos = [...state, todo(action)];
+      bake_cookie('todos', todos);
       return todos;
     case DELETE_TODO:
       todos = deleteTodoById(state, action.id);
+      bake_cookie('todos', todos);
       return todos;
     case DELETE_ALL:
       todos = [];
+      bake_cookie('todos', todos);
       return todos;
     default:
       return state;

bfe16e413fc411aeb66b08ac752b7bdbe0acb6b5
Travis Alcantara <travis.alcantara@gmail.com>
2017-04-03 17:53:49 -0700

    Completed lesson 3 up to timestamping reminders

---
+++ react-course/reminder-pro/src/components/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder, deleteReminder } from '../actions';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text:'',
+      dueDate: ''
+    }
+  }
+  addReminder() {
+    console.log('this.state.dueDate', this.state.dueDate);
+    this.props.addReminder(this.state.text, this.state.dueDate);
+  }
+  deleteReminder(id) {
+    console.log('deleting in application', id);
+    console.log('this.props', this.props);
+    this.props.deleteReminder(id);
+  }
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+        {
+          reminders.map(reminder => {
+            return (
+              <li key={reminder.id} className="list-group-item">
+                <div className="list-item">{reminder.text}</div>
+                <div
+                  className="list-item delete-button"
+                  onClick={() => this.deleteReminder(reminder.id)}
+                  >
+                  &#x2715;
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          Reminder Pro
+        </div>
+        <div className="form-inline reminder-form">
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="I have to ..."
+              onChange={event => this.setState({text:event.target.value})}
+            />
+            <input
+              className="form-control"
+              type="datetime-local"
+              onChange={event => this.setState({dueDate: event.target.value})}
+            />
+          </div>
+
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.addReminder()}
+          >
+            Add Reminder
+          </button>
+        </div>
+        { this.renderReminders() }
+      </div>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({addReminder, deleteReminder}, dispatch);
+}
+
+function mapStateToProps(state) {
+
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ react-course/reminder-pro/src/reducers/index.js
@@ -0,0 +1,33 @@
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+
+const reminder = (action) => {
+  let { text, dueDate } = action;
+  return {
+    text,
+    dueDate,
+    id: Math.random()
+  }
+}
+
+const removeById = (state = [], id) => {
+  const reminders = state.filter(reminder => reminder.id !== id);
+  console.log('new reduced reminders', reminders);
+  return reminders;
+}
+
+const reminders = (state = [], action) => {
+  let reminders = null;
+  switch(action.type) {
+    case ADD_REMINDER:
+      reminders = [...state, reminder(action)];
+      console.log('reminders as state', reminders);
+      return reminders;
+    case DELETE_REMINDER:
+      reminders = removeById(state, action.id);
+      return reminders;
+    default:
+      return state;
+  }
+}
+
+export default reminders;

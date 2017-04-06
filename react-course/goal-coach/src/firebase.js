403172e30a651ccffc001a8ae9a7d79c6e3693b9
Travis Alcantara <travis.alcantara@gmail.com>
2017-04-05 20:12:50 -0700

    completed lesson 4 up through adding firebase

---
+++ react-course/goal-coach/src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyBk8sB_WuI5q6XcliPnZgHPL_bp3PnWHng",
+    authDomain: "goalcoach-e12da.firebaseapp.com",
+    databaseURL: "https://goalcoach-e12da.firebaseio.com",
+    projectId: "goalcoach-e12da",
+    storageBucket: "goalcoach-e12da.appspot.com",
+    messagingSenderId: "548272361890"
+};
+
+export const firebaseApp= firebase.intializeApp(config);

c9512eb28ae4c992992c77c82538411929771ffa
Travis Alcantara <travis.alcantara@gmail.com>
2017-04-28 22:07:52 -0700

    Completed section on arrays

---
+++ datastructure-course/notes.js
@@ -0,0 +1,19 @@
+// Section 2
+
+// Intro to Arrays
+
+/*
+  Array: container of elements that allows users to
+  group elements together.
+
+  arrays are objects in javascript
+  push - add element to end of array
+  pop - remove last element
+  indexOf - returns index
+
+  map returns new array
+
+  reduce returns a value
+*/
+
+//Intro to Linked Lists

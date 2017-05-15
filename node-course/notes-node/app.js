d3483540d69c2df09e3976e86bef7343908791c8
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-15 13:47:42 -0700

    Finished up to read note

--- node-course/notes-node/app.js
+++ node-course/notes-node/app.js
@@ -11,13 +11,26 @@ console.log('Command: ', command);
 console.log('Yargs', argv);
 if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
-  note === undefined ? console.log('Note title taken') : console.log('title: ' + note.title + ', body:' + note.body);
+  if (note) {
+    console.log('Note created');
+    notes.logNote(note);
+  } else {
+    console.log('Note title taken');
+  }
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('Note found');
+    notes.logNote(note);
+  } else {
+    console.log('Note note found');
+  }
 } else if (command === 'remove') {
-  notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
 } else {
   console.log('Command not recognized');
 }
--- node-course/notes-node/notes.js
+++ node-course/notes-node/notes.js
@@ -39,16 +39,29 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-  console.log('Getting note', title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
 }
 
 var removeNote = (title) => {
-  console.log('Removing note', title);
+  var notes = fetchNotes();
+  var updatedNotes = notes.filter((note) => note.title !== title);
+  saveNotes(updatedNotes);
+
+  return notes.length !== updatedNotes.length;
 }
+
+var logNote = (note) => {
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+};
 
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };

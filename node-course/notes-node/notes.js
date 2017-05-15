68aba3014922f2d06c706c98d69c427a8016ac01
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-15 14:10:50 -0700

    finished first debugging video

--- node-course/notes-node/notes.js
+++ node-course/notes-node/notes.js
@@ -53,6 +53,7 @@ var removeNote = (title) => {
 }
 
 var logNote = (note) => {
+  debugger;
   console.log('--');
   console.log(`Title: ${note.title}`);
   console.log(`Body: ${note.body}`);
---
+++ node-course/notes-node/playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+  name: 'Travis'
+};
+
+person.age = 29;
+
+debugger;
+
+person.name = 'Mike';
+
+console.log(person);

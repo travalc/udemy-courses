cc2556315f084e7a9ca20cd5d977cc2a7ca2db1f
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-17 13:02:39 -0700

    Finished get weather

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,40 +1,33 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode.js');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+const yargs = require('yargs');
 
-//deeab4b2010275e7fd928941f5f48d7d
+const geocode = require('./geocode/geocode.js');
+const weather = require('./weather/weather.js');
 
-const request = require('request');
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
-request({
-  url: 'https://api.darksky.net/forecast/deeab4b2010275e7fd928941f5f48d7d/37.6063621,-122.1178261',
-  json: true
-}, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    console.log(`It is currently ${body.currently.temperature} degrees.`);
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
   } else {
-    console.log('Unable to fetch weather.');
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
   }
 });

298c71c48d78c5a32924de1f0e3386ccbfffbd13
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-22 13:08:09 -0700

    Finished handlebars videos

--- node-course/node-web-server/server.js
+++ node-course/node-web-server/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   //res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Travis',
-    likes: ['BJJ', 'Travel']
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome Home!'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
